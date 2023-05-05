1cb7850090527616822037708774e2d5d0c2bdd7
cozy06 <jamesjin06@gmail.com>
2023-05-06 01:08:26 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,17 @@ plugins {
 }
 
 group = "com.github.cozy06"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.cozy06:conveniency:25f71ecf55")
+    implementation("org.json:json:20220924")
 }
 
 tasks.test {
@@ -21,4 +24,16 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "com.github.cozy06.MainKt"
+    }
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+    archiveFileName.set("${project.name}-${version}.jar")
+    destinationDirectory.set(file("$buildDir/libs"))
 }
